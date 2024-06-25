ab8b5d3fd0021ea2d75aa6dd90244fde1dded613
Zian Rinzler Valles <83147228+Miraku17@users.noreply.github.com>
2024-06-25 11:51:18 +0800

    updated footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="w-full">
       <div className="bg-yellowFooter py-3 flex justify-center items-center flex-col">
-        <p className="text-2xl font-bold text-center">
+        <p className="text-xl font-bold text-center">
           Are you ready to order with the best deals?
         </p>
         <button className="py-1 text-white bg-orange-600 font-bold w-80 mt-2">
@@ -21,9 +21,40 @@ const Footer = () => {
             height={36}
           />
         </div>
+        <div className="company-section text-white justify-center px-20 py-3 flex flex-row mt-3">
+          <div className="flex flex-1 flex-col">
+            <h1 className="text-xl font-bold pb-2">Company</h1>
+            <p className="text-gray-500 font-thin">About Us</p>
+            <p className="text-gray-500 font-thin">Team</p>
+            <p className="text-gray-500 font-thin">Careers</p>
+            <p className="text-gray-500 font-thin">Blog</p>
+          </div>
+          <div className="flex flex-1 flex-col">
+            <h1 className="text-xl font-bold pb-2">Contact</h1>
+            <p className="text-gray-500 font-thin">Help & Support</p>
+            <p className="text-gray-500 font-thin">Partner with us</p>
+          </div>
+          <div className="flex flex-1 flex-col">
+            <h1 className="text-xl font-bold pb-2">Legal</h1>
+            <p className="text-gray-500 font-thin">Terms & Conditions</p>
+            <p className="text-gray-500 font-thin">Refund & Cancellation</p>
+            <p className="text-gray-500 font-thin">Privacy Policy</p>
+            <p className="text-gray-500 font-thin">Cookie Policy</p>
+          </div>
+          <div className="flex flex-1 flex-col">
+            <h1 className="text-base font-bold pb-2 text-gray-700">
+              FOLLOW US
+            </h1>
+            <div className="icons flex-row flex gap-5">
+              <Image src="/insta.svg" alt="Insta Icon" width={21} height={21} />
+              <Image src="/fb.svg" alt="Fb Icon" width={21} height={21} />
+              <Image src="/twitter.svg" alt="X Icon" width={21} height={21} />
+            </div>
+          </div>
+        </div>
 
-        <p className="text-xs mt-3 text-grayText font-normal">
-          © 2024 Your Company. All rights reserved.
+        <p className="text-xs mt-5 text-grayText font-normal px-20">
+          © 2024 Kahayag. All rights reserved.
         </p>
       </div>
     </div>
