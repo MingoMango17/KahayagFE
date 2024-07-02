9eb0ec40d0005093333680f3163b3c18f99983a1
Zian Rinzler Valles <83147228+Miraku17@users.noreply.github.com>
2024-07-02 23:06:01 +0800

    added checkout confirmation and order summary

--- src/components/ordertype/OrderTypeButton.tsx
+++ src/components/ordertype/OrderTypeButton.tsx
@@ -1,14 +1,20 @@
-"use client"
+// "use client"
 
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
 
-function OrderTypeButton() {
-  const [selectedOrderType, setSelectedOrderType] = useState('Dine in')
+
+interface orderTypeProps{
+  selectedOrderType: string,
+  setSelectedOrderType: (mode:string) => void;
+}
+
+function OrderTypeButton({selectedOrderType,setSelectedOrderType}: orderTypeProps) {
+  // const [selectedOrderType, setSelectedOrderType] = useState('Dine In')
 
   return (
     <div className='w-[300px] flex justify-center'>
-        <button className={`bg-grayButton hover:opacity-90 text-gray-700 w-full rounded-l-full p-2 semi-bold ${ selectedOrderType === "Dine in" && "bg-maroon text-white"}`} onClick={() => setSelectedOrderType("Dine in")}>Dine in</button>
-        <button className={`bg-grayButton hover:opacity-90 text-gray-700 w-full rounded-r-full p-2 semi-bold ${ selectedOrderType === "Take out" && "bg-maroon text-white"}`} onClick={() => setSelectedOrderType("Take out")}>Take out</button>
+        <button className={`bg-grayButton hover:opacity-90 text-gray-700 w-full rounded-l-full p-2 semi-bold ${ selectedOrderType === "Dine In" && "bg-maroon text-white"}`} onClick={() => setSelectedOrderType("Dine In")}>Dine In</button>
+        <button className={`bg-grayButton hover:opacity-90 text-gray-700 w-full rounded-r-full p-2 semi-bold ${ selectedOrderType === "Take Out" && "bg-maroon text-white"}`} onClick={() => setSelectedOrderType("Take Out")}>Take Out</button>
     </div>
   )
 }
